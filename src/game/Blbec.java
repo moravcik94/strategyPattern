535233b48017a9251cd8e4969513ed4b701a5438
jakub.moravcik <jakub.moravcik@MORAVCIK.MIM.local>
2018-08-21 15:46:09 +0200

    Add new character

---
+++ src/game/Blbec.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author jakub.moravcik
+ */
+public class Blbec extends Character{
+
+    public Blbec() {
+        weaponBehavior = new KnifeBehavior();
+    }
+    
+}
