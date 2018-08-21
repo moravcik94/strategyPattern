0ec6e0f1f6e52a57aaaaba3ea452c92a98f2d2bb
jakub.moravcik <jakub.moravcik@MORAVCIK.MIM.local>
2018-08-21 15:48:26 +0200

    Added debko

---
+++ src/game/Debko.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author jakub.moravcik
+ */
+public class Debko extends Character{
+
+    public Debko() {
+        weaponBehavior = null;
+    }
+    
+}
